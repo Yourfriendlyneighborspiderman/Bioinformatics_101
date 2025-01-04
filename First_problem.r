90bb8a891e1cfc995240db4648e7d6b6de032d10
Yourfriendlyneighborspiderman <nickgiann2002@gmail.com>
2025-01-04 18:09:28 +0200

    First_problem.r

--- First_problem.r
+++ First_problem.r
@@ -5,22 +5,41 @@ open(standard_input)
 p=0
 m=0
 
-while(4>3)
+while(667>666)
 {
     print("Provide a coding sequence.")
-    string <- readLines(standard_input, n = 1)
-    input<-unlist(strsplit(string, split=""))
-    input1<-unlist(strsplit(string, split=""))
 
+    #We assume that the characters given are valid -capital letters, in english
+
+    #Read string from standard input
+    string <- readLines(standard_input, n = 1)
+
+    #Create a vector, that consists of the individual characters of the string given
+    input<-unlist(strsplit(string, split=""))
+
+    #The same vector in reverse (I did not use the variable input - I created input1 - for OCD reasons)
+    input1<-unlist(strsplit(string, split=""))
     reverse<-rev(input1)
 
-    if(length(input)>=9)
+    #Let's assume a coding region consists of (at least) a start codon, an end codon, and an in between codon (9 nucleotides in total)
+    if(length(input)>=9) 
     {
+        #The minus 2 is important when reaching the last characters of the vector
         for (i in 1:(length(input) - 2)) 
         {
+            #Scan for a start codon (just for fun I added the option to detect mRNA sequences not just DNA)
+            #The scanning is done by detecting three nucleotides at a time, thats why I added the minus 2 in the above for statement
+            
             if (input[i] == "A" && input[i + 1] == "T" && input[i + 2] == "G" | input[i] == "A" && input[i + 1] == "U" && input[i + 2] == "G") 
             {
+                #Save the position where the programme finds a start codon
                 p<-i
+
+                #Insted of scanning the remaining sequence three nucleotides at a time
+                #a vector is created, in each position of which, a string of three characters is saved
+                #So since we know the start codon, all that has to be done is to seperate the remaing sequence into strings of three
+                #and check if there is a stop codon in any position
+                
                 input<-input[p:length(input)]
                 input<-paste0(input, collapse="")
                 input <- unlist(strsplit(input, split="(?<=.{3})", perl=TRUE))
@@ -34,14 +53,21 @@ while(4>3)
                         print("If we were to show the codons, it would look like this: ", quote=F)
                         print(input)
                         print(" ", quote=F)
+
+                        #Had I not put a break command, a printing chaos would have ensued, that would have printed "It is a coding sequence"
+                        # a zillion times
+                        
                         break
                     
                     }
                 }
+
+            #I am not 100% sure why this break is needed, but the programme would crash without it    
             break
             }
         }
-
+        
+        #Same thing as before for the reverse sequence
         for (l in 1:(length(reverse) - 2)) 
         {
             if (reverse[l] == "A" && reverse[l + 1] == "T" && reverse[l + 2] == "G" || reverse[l] == "A" && reverse[l + 1] == "U" && reverse[l + 2] == "G") 
@@ -68,7 +94,7 @@ while(4>3)
             }
         }
     
-    
+        #There is no start codon
         if(p==0 && m==0)
         {
             print("Could not find coding sequence", quote=F)
@@ -77,6 +103,7 @@ while(4>3)
 
 
     }
+    #The minimum requirement for a coding sequence was not met
     else
     {
         print("YOU DID NOT FEED ME ENOUGH NUCLEOTIDES, GOODBYE FOREVER", quote=F)
